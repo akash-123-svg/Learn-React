3762e17d0037cf8e04763670676f81dc7c4694a7
Akash keshri <keshriakash522@gmail.com>
2023-06-10 23:38:36 +0530

    episode-03

--- App.js
+++ App.js
@@ -14,7 +14,7 @@
  */
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
@@ -27,8 +27,99 @@ const parent=React.createElement("div",{id:"parent"},[
       ]),
    ]);
    
-   // JSX
    
-   console.log(parent); // return object
-   const root=ReactDOM.createRoot(document.getElementById("root"));
-   root.render(parent);
+  //  console.log(parent); // return object
+  //  const root=ReactDOM.createRoot(document.getElementById("root"));
+  //  root.render(parent);
+
+
+   /**Laying the foundation
+    *
+    * React.createElement => ReactElemet - Js object => HTMLElement(render)
+    * 
+    */
+
+  
+   const heading = React.createElement("h1",
+   {id: "heading"},
+   "Namaste React 🚀");
+
+  
+   // JSX => is not HTML in js but its looks HTML-Like or XML-like syntax 
+   // JSX (code transpiled before it reaches the js) - PARCEL - Babel(this is a normal package which converts jsx to react code)
+   // JSX => Bables transpiled it to React.createElement => ReactElemet - Js object => HTMLElement(render)
+
+   // React Element
+   const jsxheading= (
+   <h1 className="heading" tabIndex="5">
+    Namaste React using JSX 🚀</h1> 
+   );
+  
+   /**
+    * React Component
+    * 1. class based component - Old way to write the code
+    * 2. functional component - New way to write the code
+    * 
+    * 
+    */
+
+   // React functional Component - is a function that return some piece of jsx
+   const Headingcomponent = () => {
+      return <h1 className="heading">Namaste React functional component</h1>;
+   };
+
+   // OR
+
+   const Headingcomponent2 = () => (
+     <h1 className="heading">Namaste React functional component</h1>
+   );
+   
+
+   // component composition
+   const Title = () => {
+       return <h1>My Name is Akash</h1>;
+   };
+
+   const Keshri  = () => (
+      <div id="container">  
+        <Title /> 
+        <h1>I'm a software developer</h1>
+      </div>
+   ); 
+   // we can <Title /> or <Title></Title> or {Title()}
+
+   // How I put React Element inside my React component...
+   const Name = () => (
+    <div id="container">
+          {jsxheading}
+          <h1>I'm a software developer</h1>
+      </div>  
+   );
+
+   // How I put React Element inside React Element...
+   const elem = <h1>React Element</h1>
+   const style = (
+     <div id="container">
+     <h1>Namaste React 🚀</h1>
+     {elem}
+     </div>
+   );
+   
+   // React Fragment - behave like an empty tag..
+   const Ram = () => (
+    <React.Fragment>
+     <div id="container">
+      <Title />
+      <h1 className="heading">Namaste</h1>
+        </div>
+        <div id="container2">
+      </div>
+     </React.Fragment>
+   );
+
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+
+   root.render(<Ram />);
+
+
+
